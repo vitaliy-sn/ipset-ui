5e5e62ab3655c9a02d1c7399929f18c1bec61bbd
Vitalii Sn <32643620+vitaliy-sn@users.noreply.github.com>
2025-12-26 02:26:25 +0300

    Update create.go

--- internal/ipset/create.go
+++ internal/ipset/create.go
@@ -1,10 +1,14 @@
 package ipset
 
 import (
+	"fmt"
 	"os/exec"
+
+	"ipset-ui/internal/logger"
 )
 
 func Create(setName string) error {
+	logger.Info("Executing", "command", fmt.Sprintf("ipset create %s hash:net comment", setName))
 	cmd := exec.Command("ipset", "create", setName, "hash:net", "comment")
 
 	err := cmd.Run()
